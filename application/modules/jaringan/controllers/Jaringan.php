fb102d83dd02ca0d3ed4aabd42ca3a6943ebb63c
hikmat20 <hikmataulia20@gmail.com>
2020-04-15 12:26:13 +0700

    add-jaringan

--- application/modules/jaringan/controllers/Jaringan.php
+++ application/modules/jaringan/controllers/Jaringan.php
@@ -1,8 +1,9 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Jaringan extends MY_Controller {
-	
+class Jaringan extends MX_Controller
+{
+
 
 	public function index()
 	{
@@ -10,32 +11,30 @@ class Jaringan extends MY_Controller {
 		// $this->load->view('jaringan',$data);
 
 	}
-	
+
 	public function tree()
 	{
 		if ($this->uri->segment(3)) {
-			$query =$this->db->get_where('members',['username' => $this->uri->segment(3)])->result();
-		}else {
-			$query =$this->db->get('members')->result();
+			$query = $this->db->get_where('members', ['username' => $this->uri->segment(3)])->result();
+		} else {
+			$query = $this->db->get('members')->result();
 		}
 		$data['members'] = json_encode($query);
-		$this->load->view('jaringan',$data);
+		$this->load->view('jaringan', $data);
 	}
-	
+
 	public function member()
 	{
 		if ($this->uri->segment(3)) {
-			$query =$this->db->get_where('members',['upline' => 'NGS518457'])->result();
-		}else {
-			$query =$this->db->get('members')->result();
+			$query = $this->db->get_where('members', ['upline' => 'NGS518457'])->result();
+		} else {
+			$query = $this->db->get('members')->result();
 		}
 		return json_encode($query);
 	}
-	
+
 	public function checkMember($userId)
 	{
 		return $this->db->get_where('member', ['memberId' => $userId])->num_rows();
 	}
-
 }
-
