9cb0219b6d8509461ead412e01a8626f86046614
hikmat20 <hikmataulia20@gmail.com>
2020-04-16 00:28:03 +0700

    modified menu

--- application/libraries/Template.php
+++ application/libraries/Template.php
@@ -15,6 +15,8 @@ class Template
 		 * variabel diatas ^ nantinya akan dikirim ke file views/template/index.php
 		 * */
         $data['headernya'] = $this->_ci->load->view('template/header', $data, TRUE);
+        $data['navbar'] = $this->_ci->load->view('template/navbar', $data, TRUE);
+        $data['sidebar'] = $this->_ci->load->view('template/sidebar', $data, TRUE);
         $data['contentnya'] = $this->_ci->load->view($content, $data, TRUE);
         $data['footernya'] = $this->_ci->load->view('template/footer', $data, TRUE);
 
--- application/modules/template/views/index.php
+++ application/modules/template/views/index.php
@@ -19,118 +19,12 @@
 </head>
 
 <!-- <body class="hold-transition sidebar-mini"> -->
+
 <body class="sidebar-mini control-sidebar-slide-open text-sm">
 	<div class="wrapper">
-		<nav class="main-header navbar navbar-expand navbar-dark navbar-light">
-			<!-- Left navbar links -->
-			<ul class="navbar-nav">
-				<li class="nav-item">
-					<a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-				</li>
-			</ul>
 
-			<!-- Right navbar links -->
-			<ul class="navbar-nav ml-auto">
-				<!-- Messages Dropdown Menu -->
-				<li class="nav-item dropdown">
-					<a class="nav-link" data-toggle="dropdown" href="#">
-						<i class="far fa-comments"></i>
-						<span class="badge badge-danger navbar-badge">3</span>
-					</a>
-				</li>
-
-				<!-- Notifications Dropdown Menu -->
-				<li class="nav-item">
-					<a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
-						<i class="fas fa-sign-out-alt"></i>
-					</a>
-				</li>
-			</ul>
-		</nav>
-		<!-- /.navbar -->
-
-		<!-- Main Sidebar Container -->
-		<aside class="main-sidebar sidebar-dark-maroon elevation-4">
-			<!-- Brand Logo -->
-			<a href="../../index3.html" class="brand-link text-center">
-				<!-- <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
-				<span class="brand-text text-center font-weight-light">PT. NGS</span>
-			</a>
-
-			<!-- Sidebar -->
-			<div class="sidebar">
-				<!-- Sidebar user (optional)
-				<div class="user-panel mt-3 pb-3 mb-3 d-flex">
-					<div class="image">
-						<img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-					</div>
-					<div class="info">
-						<a href="#" class="d-block">Alexander Pierce</a>
-					</div>
-				</div> -->
-
-				<!-- Sidebar Menu -->
-				<nav class="mt-2">
-					<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-						<!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
-						<li class="nav-item">
-				            <a href="<?= base_url() ?>" class="nav-link active">
-							<i class="nav-icon fas fa-tachometer-alt"></i>
-				              <p>
-				                Dashboard
-				              </p>
-				            </a>
-				         </li>
-						<li class="nav-item">
-				            <a href="<?= base_url('members') ?>" class="nav-link">
-							<i class="nav-icon fas fa-users"></i>
-				              <p>
-				                Members
-				              </p>
-				            </a>
-				         </li>
-						<li class="nav-item has-treeview">
-							<a href="#" class="nav-link">
-								<i class="nav-icon fas fa-tachometer-alt"></i>
-								<p>
-									Pengaturan
-									<i class="right fas fa-angle-left"></i>
-								</p>
-							</a>
-							<ul class="nav nav-treeview ">
-								<li class="nav-item">
-									<a href="<?= base_url('perusahaan') ?>" class="nav-link navbar-dark">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Data Perusahaan</p>
-									</a>
-								</li>
-								<li class="nav-item">
-									<a href="../../index2.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Bonus</p>
-									</a>
-								</li>
-								<li class="nav-item">
-									<a href="../../index3.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Reward</p>
-									</a>
-								</li>
-								<li class="nav-item">
-									<a href="../../index3.html" class="nav-link">
-										<i class="far fa-circle nav-icon"></i>
-										<p>Harga PIN</p>
-									</a>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</nav>
-				<!-- /.sidebar-menu -->
-			</div>
-			<!-- /.sidebar -->
-		</aside>
-
+		<?= $navbar; ?>
+		<?= $sidebar; ?>
 		<!-- Content Wrapper. Contains page content -->
 		<div class="content-wrapper">
 			<!-- Content Header (Page header) -->
